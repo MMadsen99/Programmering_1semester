40b302becabb37224bbef59f9ace783c13e1cb14
MMadsen99 <math9082@edu.easj.dk>
2020-09-30 14:59:44 +0200

    startet på Opgave5_2

---
+++ src/kapitel5/OpgaverIKapitel5/Opgave5_2.java
@@ -0,0 +1,15 @@
+package kapitel5.OpgaverIKapitel5;
+
+
+import java.util.Scanner;
+
+public class Opgave5_2 {
+    public static void main(String[] args) {
+        int Number1 = (int)Math.random() * 100;
+        int Number2 = (int)Math.random() * 100;
+
+        System.out.println("Hvad er " + Number1 + "minius " + Number2 + "?");
+
+        System.out.println(Number1 + Number2);
+    }
+}
