bfb4f6574cbeb3e7f7abd70ce9765ddc581e41a5
MMadsen99 <math9082@easj.edu.dk>
2020-10-07 20:57:42 +0200

    startet 5_3 (regnefejl i linje 11)

---
+++ src/kapitel5/OpgaverIKapitel5/Opgave5_3.java
@@ -0,0 +1,20 @@
+package kapitel5.OpgaverIKapitel5;
+
+public class Opgave5_3 {
+    public static void main(String[] args) {
+        //overskriften
+        System.out.println("Celsius \t\t\tFahrenheit");
+        System.out.println("-------------------------------");
+        System.out.println("\t\t");
+
+        for (int k = 0; k <= 100; k++) {
+            double i = k * 9 / 5 + 32;
+            System.out.println("\t" + k + "\t\t\t\t" + (double)i);
+
+        }
+
+            
+        }
+    }
+
+
---
+++ src/kapitel5/OpgaverIKapitel5/Opgave5_4.java
@@ -0,0 +1,4 @@
+package kapitel5.OpgaverIKapitel5;
+
+public class Opgave5_4 {
+}
