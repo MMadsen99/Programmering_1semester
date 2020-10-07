a4a44586fafaf3e27f47eb7cb9277e562bf9936f
MMadsen99 <math9082@easj.edu.dk>
2020-10-07 17:23:34 +0200

    lavet opgave 3_1 //TODO At prtine ud hvor roots er

---
+++ src/kapitel3/OpgaverIKapitel3/Opgave3_1.java
@@ -0,0 +1,37 @@
+package kapitel3.OpgaverIKapitel3;
+
+import java.util.Scanner;
+
+public class Opgave3_1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a value for a : ");
+
+        double a = scanner.nextDouble();
+
+        System.out.println("Enter a value for b : ");
+
+        int b = scanner.nextInt();
+
+        System.out.println("Enter a value for c : ");
+
+        int c = scanner.nextInt();
+
+        if (-b + (Math.sqrt(Math.pow(b, 2) - 4 * (int)a * c)) / 2 * (int)a >= 1) {
+            System.out.println("The equation has 2 roots");
+        } else if (-b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a >= 0) {
+            System.out.println("The equation has 1 root");
+        } else {
+            System.out.println("The equation has 0 roots");
+        }
+    }
+}
+
+//          if (x == 1){
+//            System.out.println(" x er 1");
+//        }else{
+//            System.out.println(" x er ikke 1");
+//        }
---
+++ src/kapitel6/Opgave6_2/Opgave6_2.java
@@ -0,0 +1,20 @@
+package kapitel6.Opgave6_2;
+
+import java.util.Scanner;
+
+public class Opgave6_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.println(n);
+        long j = n % 100;
+        long k = j % 10;
+        long l = k % 1;
+        System.out.println(j);
+        System.out.println(k);
+
+
+    }
+
+}
