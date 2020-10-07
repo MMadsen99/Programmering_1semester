bdef7166933aa2769e24ebee2b1716bc5555be5b
MMadsen99 <math9082@easj.edu.dk>
2020-10-07 20:36:35 +0200

    rettet opgave 5_2 //TODO ( den ville have 2 inputs istedet for 1) Tror det er ved whileloopet den går istykker

--- src/kapitel5/OpgaverIKapitel5/Opgave5_2.java
+++ src/kapitel5/OpgaverIKapitel5/Opgave5_2.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 
 public class Opgave5_2 {
     public static void main(String[] args) {
-        int Number1 = (int)Math.random() * 100;
-        int Number2 = (int)Math.random() * 100;
+        double Number1 = Math.random() * 100;
+        double Number2 = Math.random() * 100;
+        double svar = ((int)Number1 - (int)Number2);
 
-        System.out.println("Hvad er " + Number1 + "minius " + Number2 + "?");
+        System.out.println("Hvad er " + (int)Number1 + "minius " + (int)Number2 + "?");
 
-        System.out.println(Number1 + Number2);
+        Scanner input = new Scanner(System.in);
+
+        if (input.nextInt() == svar)
+            System.out.println("Du regnede rigtigt");
+
+
+        while (input.nextInt() != (int)svar) {
+
+            System.out.println(" Prøv igen");
+        }
     }
 }
