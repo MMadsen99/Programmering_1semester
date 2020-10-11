241b1e50ea8950c61ce66496cfbd8cf0688e854e
MMadsen99 <math9082@edu.easj.dk>
2020-10-11 22:42:03 +0200

    igang med opgave 5_9

--- src/kapitel5/OpgaverIKapitel5/Opgave5_9.java
+++ src/kapitel5/OpgaverIKapitel5/Opgave5_9.java
@@ -6,29 +6,22 @@ public class Opgave5_9 {
 
 
     public static void main(String[] args) {
-        Scanner StudentsNumber = new Scanner(System.in); // input fra brugeren ( hvor mange elever der er)
-        Scanner name = new Scanner(System.in); // får input fra brugeren (navene på eleverne)
 
-        System.out.println("Hvor mange studenter er der? ");
+        Scanner input = new Scanner(System.in);
 
-        int numberOfStudents = StudentsNumber.nextInt();
-        String Studentnames = " ";
-
-
-        System.out.println("Hvad hedder studenterne?");
+        System.out.println("Hvor mange studenter er der");
+        int numberOfStudents = input.nextInt();
+        int score;
+        String name = " ";
 
         for (int i = 0; i < numberOfStudents; i++) {
             System.out.print(
                     "Student: " + (i + 1) + "\n   Name: ");
-            name = name.next();
+            name = input.next();
             System.out.print("   Score: ");
-
-
+            score = input.nextInt();
 
         }
-
-
-        System.out.println("der er" + numberOfStudents + "Studerende" + " og deres navne er" + Studentnames);
 
 
     }
