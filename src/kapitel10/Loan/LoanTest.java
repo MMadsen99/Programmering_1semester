b041519b86295b7e252eeb16a8d6bdddb8a5caed
MMadsen99 <math9082@easj.edu.dk>
2020-11-04 18:39:42 +0100

    opgave 10.1 + Test (in progress)

---
+++ src/kapitel10/Loan/LoanTest.java
@@ -0,0 +1,45 @@
+package kapitel10.Loan;
+
+class LoanTest {
+
+    @org.junit.jupiter.api.Test
+    void getAnnualInterestRate() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAnnualInterestRate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumberOfYears() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNumberOfYears() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLoanAmount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLoanAmount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMonthlyPayment() {
+        Loan loan1 = new Loan();
+
+        loan1.getMonthlyPayment();
+        assertEquals();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalPayment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLoanDate() {
+    }
+}
