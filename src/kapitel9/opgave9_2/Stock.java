521f68e8f6be60f19033f109d86c6b7ed4b7acf3
MathiasMadsen <m1999@live.dk>
2020-11-05 21:08:53 +0100

    opgave 9_2 inprogress

---
+++ src/kapitel9/opgave9_2/Stock.java
@@ -0,0 +1,28 @@
+package kapitel9.opgave9_2;
+
+
+
+public class Stock {
+    String symbol = " "; // String med navn symbol, indeholder ikke noget
+    String name = " ";// String med navn name, indeholder ikke noget
+    Stock stock1 = new Stock();
+    double previousClosingPrice = 9000;
+    double currentPrice = 10000;
+
+    public static void main(String[] args) {
+
+    }
+
+    Stock() {
+        symbol = "$";
+        name = "Tesla";
+
+    }
+}
+
+public void getChangePercent(){
+
+
+
+}
+
