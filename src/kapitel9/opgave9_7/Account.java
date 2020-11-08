a8fce61b5be75e4f1ff7593503a749bc2db0cff8
MathiasMadsen <m1999@live.dk>
2020-11-08 13:00:29 +0100

    opgave 9_7 added getMonthlyRate and getMonthlyInterest //TODO add date, withdraw and deposit function

--- src/kapitel9/opgave9_7/Account.java
+++ src/kapitel9/opgave9_7/Account.java
@@ -1,13 +1,14 @@
 package kapitel9.opgave9_7;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 
 public class Account {
     private int id; // id for the account
-    private double balance; // how much money the account have
-    private double annualInterestRate; // all accounts have the same intrest rate
+    private static double balance; // how much money the account have
+    private static double annualInterestRate; // all accounts have the same intrest rate
     private Date dateCreated; // stores the date the account was created
-
+    //static DecimalFormat decFormat = new DecimalFormat("0.000"); //TODO fix decimals
     public static void main(String[] args) {
 
         Account account1 = new Account();
@@ -19,8 +20,16 @@ public class Account {
         Account account2 = new Account(2,15000, 2);
         account2.printAccountDetails();
 
-        Account account3 = new Account();
+
+        Account account3 = new Account(3, 100000,2);
         account3.printAccountDetails();
+
+        System.out.println("The monthly interest rate is: " + Account.getMonthlyInterestRate() + " for all accounts");
+
+        getMonthlyInterest();
+
+
+
     }
     // no-arg constructor ( No-argument constructor)
     Account(){
@@ -61,6 +70,22 @@ public class Account {
         return dateCreated;
     }
 
+    public static double getMonthlyInterestRate(){
+
+        double monthlyInterestRate = annualInterestRate /12;
+        //  decFormat.format(monthlyInterestRate); //TODO  fix decimals
+
+        return monthlyInterestRate;
+    }
+
+    public static double getMonthlyInterest(){
+
+        double monthlyinterestNumber = ((getMonthlyInterestRate() * balance) + balance);
+
+        System.out.println(monthlyinterestNumber);
+
+        return monthlyinterestNumber;
+    }
 
     public void printAccountDetails(){
         System.out.println("id: " + getId() + "\n" + "balance: " + getBalance() + "\n" + "the annual interest rate is: " + getAnnualInterestRate());
