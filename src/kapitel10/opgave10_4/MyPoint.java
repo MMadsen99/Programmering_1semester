bddf8c42ccccc630308c07be4cb543ac6c8e4cf3
MathiasMadsen <m1999@live.dk>
2020-11-15 15:19:22 +0100

    10.4 first 3 points done, working on distance method.

--- src/kapitel10/opgave10_4/MyPoint.java
+++ src/kapitel10/opgave10_4/MyPoint.java
@@ -1,4 +1,38 @@
 package kapitel10.opgave10_4;
 
 public class MyPoint {
+    int x;
+    int y;
+
+
+    public static void main(String[] args) {
+        MyPoint mp = new MyPoint(5,1);
+      distance(mp);
+       MyPoint mp1 = new MyPoint();
+    }
+
+
+    MyPoint(){
+        x = 0;
+        y = 0;
+        System.out.println("The point is " + "(" + x + "." + y + ")");
+
+    }
+
+    MyPoint(int x,int y){
+        System.out.println("The point is " + "(" + x + "." + y + ")");
+
+    }
+    public static void distance(MyPoint  mp1){
+
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
