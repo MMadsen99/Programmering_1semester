d2c65300bc081a6ac539304026e72bfb13e49b56
MathiasMadsen <m1999@live.dk>
2020-11-15 16:03:23 +0100

    //TODO 10.4

--- src/kapitel10/opgave10_4/MyPoint.java
+++ src/kapitel10/opgave10_4/MyPoint.java
@@ -1,31 +1,43 @@
 package kapitel10.opgave10_4;
 
 public class MyPoint {
-    int x;
-    int y;
+    double x;
+    double y;
 
 
     public static void main(String[] args) {
-        MyPoint mp = new MyPoint(5,1);
-      distance(mp);
-       MyPoint mp1 = new MyPoint();
+        MyPoint point1 = new MyPoint();
+        MyPoint newPoint = new MyPoint(5,1);
+
+
+        System.out.println("The distance between (" + (int)point1.x + "," + (int)point1.y + ") and (" + (int)newPoint.x + "," + (int)newPoint.y + ")");
+        System.out.println("is " + distance(newPoint));
     }
 
 
     MyPoint(){
-        x = 0;
-        y = 0;
-        System.out.println("The point is " + "(" + x + "." + y + ")");
+     this(0,0);
 
     }
 
     MyPoint(int x,int y){
-        System.out.println("The point is " + "(" + x + "." + y + ")");
+        this.x = x;
+        this.y = y;
 
     }
-    public static void distance(MyPoint  mp1){
+    public static double distance(MyPoint  newPoint){
 
+        // d = distance between the two points
 
+        double d = Math.sqrt((Math.pow(newPoint.getX() - newPoint.x,2) + Math.pow(newPoint.x - newPoint.getY(),2)));
+
+        return d;
+
+    }
+
+    public double distance(double x, double y){
+
+        return distance(new MyPoint((int)x,(int)y));
     }
 
     public double getX() {
