680a2b9792a3362212d36cd8c93173597ae37e97
MathiasMadsen <m1999@live.dk>
2020-11-20 09:45:12 +0100

    11.4 Arraylist made and filled

---
+++ src/kapitel11/opgave11_4/opgave11_4.java
@@ -0,0 +1,25 @@
+package kapitel11.opgave11_4;
+
+import java.util.ArrayList;
+
+public class opgave11_4 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        number.add(13);
+        number.add(10);
+        number.add(6);
+        number.add(9);
+        System.out.println(number.toString());
+    }
+
+
+    public static Integer max(ArrayList<Integer> number){
+
+        for (int i = 0; i < number.size(); i++) {
+
+        }
+
+        return  null;
+    }
+}
