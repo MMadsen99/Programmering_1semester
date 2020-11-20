7c3731d6c18fb0375546bb320b1658fb8fbc0cf7
MathiasMadsen <m1999@live.dk>
2020-11-20 10:01:02 +0100

    11.4  find the maximun element done

--- src/kapitel11/opgave11_4/opgave11_4.java
+++ src/kapitel11/opgave11_4/opgave11_4.java
@@ -1,6 +1,7 @@
 package kapitel11.opgave11_4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class opgave11_4 {
 
@@ -8,18 +9,23 @@ public class opgave11_4 {
         ArrayList<Integer> number = new ArrayList<Integer>();
         number.add(13);
         number.add(10);
-        number.add(6);
+        number.add(23);
         number.add(9);
         System.out.println(number.toString());
+        max(number);
+        System.out.println(number.get(0));
     }
 
 
-    public static Integer max(ArrayList<Integer> number){
+    public static ArrayList<Integer> max(ArrayList<Integer> number){
 
         for (int i = 0; i < number.size(); i++) {
+            if (number.get(0) < number.get(i)){
+                Collections.swap(number,0,i);
 
+            }
         }
 
-        return  null;
+        return number;
     }
 }
