11fdc6f7bee591a6c16eceb6d56040e1162926da
MathiasMadsen <m1999@live.dk>
2020-11-21 17:55:51 +0100

    12_2

--- src/kapitel12/opgave12_2/ArrayIndexOutOfBounds.java
+++ src/kapitel12/opgave12_2/ArrayIndexOutOfBounds.java
@@ -9,10 +9,20 @@ public class ArrayIndexOutOfBounds {
         System.out.println("To get the month and how many days that are in the month");
         int userInput = input.nextInt();
         // tilføjet en fill i begge arrays da de starter i 0 og ikke 1,
-        String months[] = { "Error","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] months = { "Error","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         int[] dom = {0 ,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         System.out.println(months[userInput] + " " + dom[userInput]);
 
+    }
+    public static void WrongNumber(){
+        try{
+
+
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Only integers between 1 and 12");
+        }
+        System.out.println("The program continues ...");
     }
 }
